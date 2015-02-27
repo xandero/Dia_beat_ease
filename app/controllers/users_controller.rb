eb8bfc089445e782b609c154d0e865686b67a0f2
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-27 18:33:28 +1100

    user CRUD 0.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
@@ -13,6 +14,26 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    user = User.find params[:id]
+    user.update user_params
+    redirect_to user_path
+  end
+
+  def destroy
+    user = User.find params[:id]
+    user.destroy
+    redirect_to users_path
   end
 
   private
