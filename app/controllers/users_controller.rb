98809f4a34a9bbc362621ee687ad7bcbbcb8ea3f
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-28 14:16:05 +1100

    log user in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create user_params
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
