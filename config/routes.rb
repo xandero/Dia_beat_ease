20029be83c420ca8e49f83529d6f442bff4dc46f
Bishin Ho <bishin.ho@gmail.com>
2015-02-28 13:07:59 +1100

    bootstrap and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root :to => 'users#index'
   resources :users, :foods, :activities, :bloodsugars
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
 end
