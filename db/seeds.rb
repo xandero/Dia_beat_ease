3375c0f00641e9edce41f34719b19c331fd3ee39
soxface <sonyacorcoran@gmail.com>
2015-02-28 14:20:44 +1100

    badder seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 User.destroy_all
-Activity.destroy_all
 
-u1 = User.create(:username=> "sox", :dob => "Fri, 30 Mar 1984", :gender => "Female", :weight => 65.0, :height => 130.0, :basal_insulin => 10.0, :bolus_insulin => 1.0, :diabetes_type => "Type 1", :is_admin => "true", :created_at => nil, :updated_at => nil, :password => "chicken", :password_confirmation => "chicken", :email => "sox@sox.com")
+u1 = User.create(:username=> "sox", :password => "chicken", :password_confirmation => "chicken", :email => "sox@sox.com")
 
 # u2 = User.create(:username => "tom", :dob => nil, :gender => "male", :weight => 65, :height => 150, :basal_insulin => 15, :bolus_insulin => 1, :diabetes_type => "Type 1", :is_admin => "TRUE", :created_at => nil, :updated_at => nil, :password_digest => "chicken", :email => "tom@tom.com")
 
