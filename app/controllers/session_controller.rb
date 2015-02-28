06d734c3fc1619a49122b1f3414a6f4eb0f3016a
soxface <sonyacorcoran@gmail.com>
2015-02-28 14:29:35 +1100

    fixed login and seed data

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,7 +4,7 @@ class SessionController < ApplicationController
   end
 
   def create
-        user = User.find_by :username =>params[:username]
+        @user = User.find_by :username =>params[:username]
         if @user.present? && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to(user_path(@user))
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,2 @@
 User.destroy_all
-
-u1 = User.create(:username=> "sox", :password => "chicken", :password_confirmation => "chicken", :email => "sox@sox.com")
-
-# u2 = User.create(:username => "tom", :dob => nil, :gender => "male", :weight => 65, :height => 150, :basal_insulin => 15, :bolus_insulin => 1, :diabetes_type => "Type 1", :is_admin => "TRUE", :created_at => nil, :updated_at => nil, :password_digest => "chicken", :email => "tom@tom.com")
-
-# u3 = User.create(:username => "bishin", :dob => nil, :gender => "female", :weight => 65, :height => 130, :basal_insulin => 10, :bolus_insulin => 1, :diabetes_type => "Type 1", :is_admin => "TRUE", :created_at => nil, :updated_at => nil, :password_digest => "chicken", :email => "bishin@bish.com"),
-
-# u4 = User.create(:username => "xander", :dob => nil, :gender => "male", :weight => 65, :height => 150, :basal_insulin => 15, :bolus_insulin => 1, :diabetes_type => "Type 1", :is_admin => "TRUE", :created_at => nil, :updated_at => nil, :password_digest => "chicken", :email => "xander@xander.com"),
-
-# a1 = Activity.create(:activityname =>"jog", :duration =>60, :intensity =>4, :activity_time =>nil, :created_at =>nil, :updated_at =>nil, :user_id =>nil)
+u1 = User.create(:username=> "sox", :dob => "Fri, 30 Mar 1984", :gender => "Female", :weight => 65.0, :height => 130.0, :basal_insulin => 10.0, :bolus_insulin => 1.0, :diabetes_type => "Type 1", :is_admin => "false", :created_at => nil, :updated_at => nil, :password => "chicken", :password_confirmation => "chicken", :email => "sox@sox.com")
