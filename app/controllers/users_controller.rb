0e450b1235f84626c14c8b6e09915a8c0a339586
Tom Valorsa <tomvalorsa@gmail.com>
2015-02-28 14:18:25 +1100

    sign up comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create user_params
     if @user.save
+      # logs user in automatically upon signup
       session[:user_id] = @user.id
       redirect_to root_path
     else
