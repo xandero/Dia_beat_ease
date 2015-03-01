7fd8837e26fcdcc8521f28c55178c8b69945f774
xandero <alexander.ohan@gmail.com>
2015-03-01 13:30:22 +1100

    initial JSON code for weather

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,34 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+//function getWeather(lat,long){
+
+function getWeather() {
+  var apiKey = '0bc0203aed2cfca106f903a48670b4c4';
+  var url = 'https://api.forecast.io/forecast/';
+  var lati = 37.8267;
+  var longi = -122.423;
+  //var lati = lat;
+  //var longi = long;
+  var data;       
+  //units=ca makes temps in celcius and wind speed in KM PH
+  $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?&units=ca", function(data) {
+    console.log(data);
+    console.log(data.daily.data);
+    $('#weather').html('time                      : ' + data.currently.time +
+     '<br>summary               : ' + data.currently.summary +
+     '<br>timezone              : ' + data.timezone +
+     '<br>temperature           : ' + data.currently.temperature +
+     '<br>apparentTemperature   : ' + data.currently.apparentTemperature +                               
+     '<br>temperatureMax        : ' + data.daily.data[0].temperatureMax +
+     '<br>temperatureMaxTime    : ' + data.daily.data[0].temperatureMaxTime +
+     '<br>temperatureMin        : ' + data.daily.data[0].temperatureMin +
+     '<br>temperatureMinTime    : ' + data.daily.data[0].temperatureMinTime +
+     '<br>units                 : ' + data.flags.units   
+    );
+  });
+}
+$( document ).ready(function() {
+  $("#getData").click(function(){
+      var lat = $("#lat").val();
+      var long = $("#long").val();
+      getWeather(lat,long);
+  });
+});
