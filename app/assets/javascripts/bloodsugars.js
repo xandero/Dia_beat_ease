a1dea77062a6fa72ae29d9fb3861420d74591b8f
Bishin Ho <bishin.ho@gmail.com>
2015-03-02 17:51:58 +1100

    matching up with master

--- app/assets/javascripts/bloodsugars.js
+++ app/assets/javascripts/bloodsugars.js
@@ -1,10 +1,12 @@
 // DROPZONE UPLOAD CODE
-
-$(function() {
-  var bloodsugarDropzone;
-  bloodsugarDropzone = new Dropzone("#bloodsugar-dropzone");
-  return bloodsugarDropzone.on("success", function(file, responseText) {
-    var imageUrl;
-    imageUrl = responseText.file_name.url;
-  });
+$(document).ready(function(){
+    $(function() {
+      var bloodsugarDropzone;
+      bloodsugarDropzone = new Dropzone("#bloodsugar-dropzone");
+      return bloodsugarDropzone.on("success", function(file, responseText) {
+        var imageUrl;
+        imageUrl = responseText.file_name.url;
+      });
+    });
 });
+
--- app/controllers/bloodsugars_controller.rb
+++ app/controllers/bloodsugars_controller.rb
@@ -4,22 +4,6 @@ class BloodsugarsController < ApplicationController
     @bloodsugars = Bloodsugar.all
   end
 
-  def create
-
-
-
-
-     # if @bloodsugar.save!
-     #    respond_to do |format|
-     #      format.json{ render :json => @bloodsugar }
-     #    end
-     # end
-
-   end
-
-  def new
-  end
-
   def import
      Bloodsugar.import(params[:file])
      render :json => Bloodsugar.all
