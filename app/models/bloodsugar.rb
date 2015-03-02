48d96f45e8a5966ec37aa7d30d946afd0a558658
soxface <sonyacorcoran@gmail.com>
2015-03-02 16:28:55 +1100

    date and time format correct in db but error on dropzone event

--- app/models/bloodsugar.rb
+++ app/models/bloodsugar.rb
@@ -13,50 +13,18 @@
 require 'pry'
 class Bloodsugar < ActiveRecord::Base
   belongs_to :user
-  #mount_uploader :file_name, BloodsugarUploader
-
-  # def self.to_csv
-  #   CSV.generate do |csv|
-  #     csv << column_names
-  #     all.each do |media|
-  #       csv.media.attributes.values_at(*column_names)
-  #     end
-  #   end
-  # end
 
   def self.import(file)
-    # binding.pry
-    #file_path = file.file_name.file.file
-    # csv = CSV.read(file.tempfile, { :col_sep => ";" })
 
-    # Define the headers here
+    # CSV is part of rails
+    # parsing (read:splitting) our single column into individual columns
     CSV.parse(file.tempfile, :col_sep => ";", :headers => true) do |row|
-      #Bloodsugar.create! row.to_hash
-      Bloodsugar.create!(readingtime: row['Date'], bslevel: row['Result'])
-      end
+    # Bloodsugar.create! new row and << csv data to database
+    # binding.pry
+    # Date and time adds to our DB but undefined method for '+'
+    Bloodsugar.create!(readingtime: (row['Date'] + 'T' + row['Time']), bslevel: row['Result'])
+    end
 
-    # CSV.foreach(csv.to_csv, headers: true) do |row|
-    #   binding.pry
-    # end
-
-    # csv.each do |current_row|
-
-
-    #   Media.create blood_sugar => ""
-    #   binding.pry
-    # end
-    # foreach(file_path, headers: true) do |row|
-    #   binding.pry
-
-
-    #   Media.create! row.to_hash.permit
-    # end
-
-    # binding.prys
   end
-
-
-
-
 
 end
