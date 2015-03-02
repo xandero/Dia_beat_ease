07669eda39547ea4950cba9981a97ae13a4d9804
xandero <alexander.ohan@gmail.com>
2015-03-02 14:38:49 +1100

    added popup bolus and basal warnings

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,33 @@
 var validateBolus = function() {
   console.log('inside function');
   var bolus_insulin = $('#bolus-value').val();
-  if ( bolus_insulin < 10 || bolus_insulin > 30 ) {
+  if ( bolus_insulin < 10 || bolus_insulin > 24 ) {
 
     var retVal = confirm("Are you sure that's correct? That number is outside the standard range for Bolus Insulin.");
     if( retVal == true ){
-      alert("You have confirmed that the Bolus value you entered is correct.");
+      alert("You have confirmed that the Bolus Insulin value you entered is correct.");
     return true;
     } else {
-      alert("Please check your Bolus value and resubmit.");
+      alert("Please correct your Bolus value and resubmit.");
+    return false;
+    }
+  }
+};
+
+var validateBasal = function() {
+  console.log('inside function');
+  var basal_insulin = $('#basal-value').val();
+  var weight = $('#user-weight').val();
+  var target_basal = weight * 0.55
+
+  if ( basal_insulin > 1.2 * (target_basal) || basal_insulin < 0.8 * (target_basal) {
+
+    var retVal = confirm("Are you sure that's correct? That number is outside the standard range for Basal Insulin.");
+    if( retVal == true ){
+      alert("You have confirmed that the Basal Insulin value you entered is correct.");
+    return true;
+    } else {
+      alert("Please correct your Basal value and resubmit.");
     return false;
     }
   }
@@ -16,6 +35,7 @@ var validateBolus = function() {
 
 $(document).ready(function() {
   $('#profile-submit').on('click', validateBolus);
+  $('#profile-submit').on('click', validateBasal);
   console.log('hello');
 
 });
