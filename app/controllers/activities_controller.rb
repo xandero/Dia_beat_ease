28f7c40310aa3bd50c30aa15956ca77790a27a41
Bishin Ho <bishin.ho@gmail.com>
2015-03-03 19:55:07 +1100

    activities template

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,3 +1,39 @@
 class ActivitiesController < ApplicationController
+    def index
+        @activities = Activity.all
+    end
 
+    def new
+        @activity = Activity.new
+    end
+
+    def create
+        activity = Activity.create activity_params
+        redirect_to activity
+    end
+
+    def show
+        @activity = Activity.find params[:id]
+    end
+
+    def edit
+        @activity = Activity.find params[:id]
+    end
+
+    def update
+        activity = Activity.find params[:id]
+        activity.update activity_params
+        redirect_to activity
+    end
+
+    def destroy
+        activity = Activity.find params[:id]
+        activity.destroy
+        redirect_to activities_path
+    end
+
+    private
+    def activity_params
+        params.require(:activity).permit(:activityname, :duration, :intensity, :activity_time)
+    end
 end
