50cb75073fecafcb1b2b5384c45a2b2e3618a1c3
xandero <alexander.ohan@gmail.com>
2015-03-04 09:51:39 +1100

    moved geocode request ip code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
   def landing
+    @user = User.find_by :id => session[:user_id]    
+    @ip_address = request.remote_ip
+    @list = Geocoder.search @ip_address
+    @city = @list.first.city
+    @user.update(:lat => @list[0].latitude, :long => @list[0].longitude, :location => @city)
   end
 
   def about
   end
 
   def testing
-    @user = User.find_by :id => session[:user_id]    
-    @ip_address = request.remote_ip
-    @list = Geocoder.search @ip_address
-    @city = @list.first.city
-    @user.update(:lat => @list[0].latitude, :long => @list[0].longitude, :location => @city)
   end
 
   def calc
