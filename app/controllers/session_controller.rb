d4abc3f6113a55e9a7a0167d842ab5250c1ef231
xandero <alexander.ohan@gmail.com>
2015-03-04 10:02:45 +1100

    changed redirect after login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,24 +1,24 @@
 class SessionController < ApplicationController
   def new
-    redirect_to root_path
+    redirect_to '/testing'
   end
 
   def create
-        @user = User.find_by :username =>params[:username]
-        if @user.present? && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+    @user = User.find_by :username =>params[:username]
+    if @user.present? && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
 
-            # just put in for testing purposes, default page after login can be decided later
-            redirect_to '/testing'
-            #redirect_to(user_path(@user))
-        else
-            flash[:error] ="Invalid login or password"
-            redirect_to(login_path)
-        end
+        # just put in for testing purposes, default page after login can be decided later
+        redirect_to '/testing'
+        #redirect_to(user_path(@user))
+    else
+        flash[:error] ="Invalid login or password"
+        redirect_to(login_path)
+    end
   end
 
   def destroy
-        session[:user_id] = nil
-        redirect_to root_path
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
