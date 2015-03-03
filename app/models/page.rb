b526680ca97ace81b5b05fa9b6f36b9f0ae73e0d
xandero <alexander.ohan@gmail.com>
2015-03-03 22:54:21 +1100

    fixed error with day not being variable

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,31 +1,29 @@
 class Page
   require 'mandrill'
   def self.check_weather(lat, long, location, name)
-    @forecast = ForecastIO.forecast(lat, long)
-
-    puts "Hi #{name}, just letting you know about some significant temperature changes this week in #{location}. Make sure that you adjust your insulin dosage in line with your doctor's recommendations."      
+    @forecast = ForecastIO.forecast(lat, long)      
     
-    message = ""
+    message = "Hi #{name}, just letting you know about some significant temperature changes this week in #{location}. Make sure that you adjust your insulin dosage in line with your doctor's recommendations."
     6.times do |i|
 
       maxTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMax"]
       maxTempMorrow = @forecast["daily"]["data"][i]["temperatureMax"]
       minTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMin"]
       minTempMorrow = @forecast["daily"]["data"][i]["temperatureMin"]
-      time = @forecast['daily']['data'][0]['time']
+      time = @forecast['daily']['data'][i]['time']
       day = Time.at(time).strftime('%A')
 
-      if ( maxTempToday - maxTempMorrow ) > 3
+      if ( maxTempToday - maxTempMorrow ) > 2
         difference = (maxTempToday - maxTempMorrow).round(0).abs
         message += "\n This #{day}, the maximum temperature is forecast to decrease by #{difference} degrees. "
-      elsif ( maxTempMorrow - maxTempToday ) > 3
+      elsif ( maxTempMorrow - maxTempToday ) > 2
         difference = (maxTempMorrow - maxTempToday).round(0).abs
         message += "\n This #{day}, the maximum temperature is forecast to increase by #{difference} degrees. "
       end
-      if ( minTempToday - minTempMorrow ) > 3
+      if ( minTempToday - minTempMorrow ) > 2
         difference = (maxTempToday - maxTempMorrow).round(0).abs
         message += "\n This #{day}, the minimum temperature is forecast to decrease by #{difference} degrees. "
-      elsif ( maxTempMorrow - maxTempToday ) > 3
+      elsif ( maxTempMorrow - maxTempToday ) > 2
         difference = (maxTempMorrow - maxTempToday).round(0).abs
         message += "\n This #{day}, the minimum temperature is forecast to increase by #{difference} degrees. "
       end
