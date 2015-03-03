2ea7fdfe7030a3f4088146eb4aaee636a822d106
xandero <alexander.ohan@gmail.com>
2015-03-03 11:09:52 +1100

    geocoder and mandrill setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,52 +28,69 @@ class User < ActiveRecord::Base
 
   validates :username, :uniqueness => true, :presence => true
   validates :email, :uniqueness => true, :presence => true
-end
 
-def self.to_mandrill_to(users)
-  users.map{|user| {:email => user.email}}
-end
+  geocoded_by :ip_address,
+    :latitude => :lat, :longitude => :lon
+  after_validation :geocode
 
-def self.to_mandrill_merge_vars(users)
-  users.map{|user| {:rcpt => user.email, 
-  :vars => [{:name => 'first_name', 
-  :content => user.first_name}]}}
-end
+  # def self.to_mandrill_to(users)
+  #   users.map{|user| {:email => user.email}}
+  # end
 
-def check_weather
-  @forecast = ForecastIO.forecast(-33.8600, 151.2094)
+  # def self.to_mandrill_merge_vars(users)
+  #   users.map{|user| {:rcpt => user.email, 
+  #   :vars => [{:name => 'first_name', 
+  #   :content => user.first_name}]}}
+  # end
 
-  6.times do |i|
-    maxTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMax"]
-    maxTempMorrow = @forecast["daily"]["data"][i]["temperatureMax"]
-    minTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMin"]
-    minTempMorrow = @forecast["daily"]["data"][i]["temperatureMin"]
-    if ( maxTempToday - maxTempMorrow ) > 5
-      notification('Just letting you know that the maximum temperature over the bext few days is forecast to decrease by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
-    elsif ( maxTempMorrow - maxTempToday ) > 5
-      notification('Just letting you know that the maximum temperature over the bext few days is forecast to increase by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+  def check_weather(lat, long)
+    @forecast = ForecastIO.forecast(-33.8600, 151.2094)
+
+    6.times do |i|
+      maxTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMax"]
+      maxTempMorrow = @forecast["daily"]["data"][i]["temperatureMax"]
+      minTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMin"]
+      minTempMorrow = @forecast["daily"]["data"][i]["temperatureMin"]
+      if ( maxTempToday - maxTempMorrow ) > 5
+        notification('Just letting you know that the maximum temperature over the bext few days is forecast to decrease by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+      elsif ( maxTempMorrow - maxTempToday ) > 5
+        notification('Just letting you know that the maximum temperature over the bext few days is forecast to increase by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+      end
+      if ( minTempToday - minTempMorrow ) > 5
+        notification('Just letting you know that the minimum temperature over the bext few days is forecast to decrease by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+      elsif ( maxTempMorrow - maxTempToday ) > 5
+        notification('Just letting you know that the minimum temperature over the bext few days is forecast to increase by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+      end
     end
-    if ( minTempToday - minTempMorrow ) > 5
-      notification('Just letting you know that the minimum temperature over the bext few days is forecast to decrease by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
-    elsif ( maxTempMorrow - maxTempToday ) > 5
-      notification('Just letting you know that the minimum temperature over the bext few days is forecast to increase by more than 10 degrees! Be sure to adjust your insulin dosage in line with the recommendations from your doctor.')
+  end
+
+  def notification(message)
+    @user = User.find params[:id]
+
+    m = Mandrill::API.new
+      message = {
+      :subject=> "Weather Alert!",
+      :from_name=> "Diabetease",
+      :from_email=>"alerts@diabetease.com",
+      :to=>User.to_mandrill_to(User),
+      :text=>"Hi #{@user.username}, " message,
+      }
+      sending = m.messages.send message
+  end
+
+  def validate_bolus_level(bolus_insulin)
+    if @user.bolus_insulin < 10 || @user.bolus_insulin > 30
+      "Are you sure that's correct? Those figures are outside the normal range expected. Please confirm before continuing."
+    end
+  end
+
+  def validate_basal_level(basal_insulin, weight)
+    daily_insulin = 0.55 * weight
+    #expected_basal_insulin = daily_insulin * 0.40
+    if basal_insulin < (daily_insulin * 0.3)
+      "Are you sure that is correct? That figure is below the expected range for your weight. Please confirm before continuing."
+    elsif basal_insulin > (daily_insulin * 0.5)
+      "Are you sure that is correct? That figure is above the expected range for your weight. Please confirm before continuing."
     end
   end
 end
-
-def validate_bolus_level(bolus_insulin)
-  if @user.bolus_insulin < 10 || @user.bolus_insulin > 30
-    "Are you sure that's correct? Those figures are outside the normal range expected. Please confirm before continuing."
-  end
-end
-
-def validate_basal_level(basal_insulin, weight)
-  daily_insulin = 0.55 * weight
-  #expected_basal_insulin = daily_insulin * 0.40
-  if basal_insulin < (daily_insulin * 0.3)
-    "Are you sure that is correct? That figure is below the expected range for your weight. Please confirm before continuing."
-  elsif basal_insulin > (daily_insulin * 0.5)
-    "Are you sure that is correct? That figure is above the expected range for your weight. Please confirm before continuing."
-  end
-end
-
