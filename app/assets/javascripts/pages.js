493d1398e3d917d245ece873ec2eb06370739286
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-03 18:08:03 +1100

    single calc done

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,7 @@
 // JS for the throwaway calc
 
 var searchFoods = function () {
-  var query = $('#query').val();
+  var query = $('#single-calc').val();
   var nutritionixUrl = 'https://api.nutritionix.com/v1_1/search/' + query;
 
   $.getJSON(nutritionixUrl, {
@@ -29,6 +29,7 @@ var listResults = function (result) {
   });
 };
 
+// currently fucking up because it's not getting the value from the form input
 
 
 $(document).ready(function() {
@@ -46,5 +47,36 @@ $(document).ready(function() {
       searchFoods();
       return false;
     }
+  });
+
+  $('#search-results').on('click', 'a', function() {
+    $('#calc-foodname').val($(this).data('foodname'));
+    $(this).addClass('selected');
+    var item_id = $(this).data('item_id');
+
+    // get rid of other results
+    var searchResults = $('#search-results a');
+
+    _(searchResults).each(function (result) {
+      if (result.classList.contains('selected') === false) {
+        // delete it!
+        result.parentNode.remove();
+      }
+    });
+
+    var nutritionixUrl = 'https://api.nutritionix.com/v1_1/item';
+
+    $.getJSON(nutritionixUrl, {
+      id: item_id,
+      appId: "92a57023",
+      appKey: "5a11032e7168104fdfa242bd3b62e636"
+    }).done(function(result) {
+      console.log(result);
+
+      $('#calc-serving-size-qty').val(result.nf_serving_size_qty);
+      $('#calc-serving-size-weight').val(result.nf_serving_weight_grams);
+      $('#calc-serving-size-unit').val(result.nf_serving_size_unit);
+      $('#calc-carbs').val(result.nf_total_carbohydrate);
+    });
   });
 });
