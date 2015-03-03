6f400b81531dbdd83f19385507ac20fa66c8d038
xandero <alexander.ohan@gmail.com>
2015-03-03 21:12:22 +1100

    fixed bug in mailer message

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,7 @@ class Page
 
     puts "Hi #{name}, just letting you know about some significant temperature changes this week in #{location}. Make sure that you adjust your insulin dosage in line with your doctor's recommendations."      
     
+    message = ""
     6.times do |i|
 
       maxTempToday = @forecast["daily"]["data"][(i-1)]["temperatureMax"]
@@ -16,19 +17,20 @@ class Page
 
       if ( maxTempToday - maxTempMorrow ) > 3
         difference = (maxTempToday - maxTempMorrow).round(0).abs
-        print "This #{day}, the maximum temperature is forecast to decrease by #{difference} degrees. "
+        message += \n "This #{day}, the maximum temperature is forecast to decrease by #{difference} degrees. "
       elsif ( maxTempMorrow - maxTempToday ) > 3
         difference = (maxTempMorrow - maxTempToday).round(0).abs
-        print "This #{day}, the maximum temperature is forecast to increase by #{difference} degrees. "
+        message += \n "This #{day}, the maximum temperature is forecast to increase by #{difference} degrees. "
       end
       if ( minTempToday - minTempMorrow ) > 3
         difference = (maxTempToday - maxTempMorrow).round(0).abs
-        print "This #{day}, the minimum temperature is forecast to decrease by #{difference} degrees. "
+        message += \n "This #{day}, the minimum temperature is forecast to decrease by #{difference} degrees. "
       elsif ( maxTempMorrow - maxTempToday ) > 3
         difference = (maxTempMorrow - maxTempToday).round(0).abs
-        print "This #{day}, the minimum temperature is forecast to increase by #{difference} degrees. "
+        message += \n "This #{day}, the minimum temperature is forecast to increase by #{difference} degrees. "
       end
     end
+    message
   end
 
   def self.notification(message, email, name)
