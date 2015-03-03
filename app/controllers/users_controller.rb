0a7e0f5af2cb8162426bb6058ab21bdcd4e3736e
xandero <alexander.ohan@gmail.com>
2015-03-03 16:49:24 +1100

    testing ip on edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,17 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     # @locate = request.remote_ip
  
+   
+  end
+
+  def edit
+    @user = User.find params[:id]
+
     @ip_address = request.remote_ip
     @list = Geocoder.search @ip_address
     @city = @list.first.city
 
     @user.update(:lat => @list[0].latitude, :long => @list[0].longitude)
-  end
-
-  def edit
-    @user = User.find params[:id]
   end
 
   def update
