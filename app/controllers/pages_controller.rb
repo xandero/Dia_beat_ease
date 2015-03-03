a60a4f670c77b9357a4c47b66cd40c129691f62a
xandero <alexander.ohan@gmail.com>
2015-03-03 17:46:58 +1100

    moved IP code to testing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
   def landing
-    @user = User.find params[:id]
-    @ip_address = request.remote_ip
-    @list = Geocoder.search @ip_address
-    @city = @list.first.city
-    @user.update(:lat => @list[0].latitude, :long => @list[0].longitude)
   end
 
   def about
   end
 
   def testing
+    @user = User.find params[:id]
+    @ip_address = request.remote_ip
+    @list = Geocoder.search @ip_address
+    @city = @list.first.city
+    @user.update(:lat => @list[0].latitude, :long => @list[0].longitude)
   end
 
   def calc
