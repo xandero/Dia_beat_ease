9fd521cdf5b851435ba57952cd7e739b20743196
xandero <alexander.ohan@gmail.com>
2015-03-05 09:46:32 +1100

    iframe fix goodness

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module DiaBeatEase
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_dispatch.default_headers.clear
   end
 end
