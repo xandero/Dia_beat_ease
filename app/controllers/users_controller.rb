6d4867da9ca745970975026af6c0c5af507fc2bf
xandero <alexander.ohan@gmail.com>
2015-03-04 19:20:11 +1100

    fixing login user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,11 @@ class UsersController < ApplicationController
   end
 
   def dashboard
+     @user = User.find_by :id => session[:user_id]    
+    @ip_address = request.remote_ip
+    @list = Geocoder.search @ip_address
+    @city = @list.first.city
+    @user.update(:lat => @list[0].latitude, :long => @list[0].longitude, :location => @city)  
   end
 
   def readingtime
