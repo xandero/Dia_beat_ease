3381e203c46dc67deeb0b0e045c37cedfdd787e7
xandero <alexander.ohan@gmail.com>
2015-03-04 18:45:04 +1100

    fixed login redirect path

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,16 +1,16 @@
 class SessionController < ApplicationController
   def new
-    redirect_to '/testing'
   end
 
   def create
     @user = User.find_by :username =>params[:username]
     if @user.present? && @user.authenticate(params[:password])
         session[:user_id] = @user.id
-
+binding.pry
         # just put in for testing purposes, default page after login can be decided later
         redirect_to '/testing'
         #redirect_to(user_path(@user))
+
     else
         flash[:error] = "Invalid login or password"
         redirect_to(login_path)
