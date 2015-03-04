8f2d990780e39b42134e34d9e822d8f965cd9ba3
soxface <sonyacorcoran@gmail.com>
2015-03-04 12:28:06 +1100

    database will not accept duplicate data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303095217) do
+ActiveRecord::Schema.define(version: 20150303001135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150303095217) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "total_carbs"
+    t.datetime "meal_date"
   end
 
   create_table "users", force: :cascade do |t|
