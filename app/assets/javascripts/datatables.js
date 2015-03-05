bea356cbdd4c312c97335e63387d65e4394864a3
Bishin Ho <bishin.ho@gmail.com>
2015-03-05 18:10:25 +1100

    datatables ajax error resolved

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,8 +1,5 @@
 $(document).ready( function () {
     $('#bloodsugar-table').DataTable({
         "lengthMenu": [ 10, 25, 50, 75, 100 ],
-         "ajax": "data.json"
-        // "paging": true
-        // "pageLength": 50
     });
 } );
