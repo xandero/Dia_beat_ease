4ed50c6d93fffc678872d4bd8c914b6473a33dfb
soxface <sonyacorcoran@gmail.com>
2015-03-05 15:24:52 +1100

    nav active class working

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,8 +1,18 @@
-  $(document).ready(function() {
-    // debugger;
-     $(".side-nav").on("click", "li", function(event){
-      event.preventDefault();
-     $(this).find(".active").removeClass("active");
-     $(this).parent().addClass("active");
-    });
-  });
+console.log('included')
+$(document).ready(function() {
+  $('.active').removeClass('active');
+  var page = $('body').attr('class');
+  if (page === 'meals-index') {
+    $('.addmeal-link').closest('li').addClass('active');
+  } else if (page === 'users-dashboard') {
+        $('.dashboard-link').closest('li').addClass('active');
+  } else if (page === 'bloodsugars-index') {
+        $('.import-link').closest('li').addClass('active');
+  } else if (page === 'activities-index') {
+        $('.addactivity-link').closest('li').addClass('active');
+  } else if (page === 'pages-weather') {
+        $('.weather-link').closest('li').addClass('active');
+  } else if (page === 'pages-calc') {
+        $('.calculator-link').closest('li').addClass('active');
+  }
+});
