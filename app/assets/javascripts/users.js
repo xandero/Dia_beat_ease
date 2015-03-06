8efc1acb30a32ec822dfaca6528fafa2d506c24d
Bishin Ho <bishin.ho@gmail.com>
2015-03-06 12:18:47 +1100

    basal and bosal ranges

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ var validateBolus = function() {
   var bolus_insulin = parseInt( $('#bolus-value').val() );
   var result = true;
 
-  if ( bolus_insulin < 10 || bolus_insulin > 24 ) {
+  if ( bolus_insulin < 0.3 || bolus_insulin > 5 ) {
     var retVal = confirm("Are you sure that's correct? That number is outside the standard range for Bolus Insulin.");
     if( retVal === true ){
       alert("You have confirmed that the Bolus Insulin value you entered is correct.");
@@ -22,7 +22,7 @@ var validateBasal = function() {
   var target_basal = weight * 0.55; 
 
   var result_basal = false;
-  if ( basal_insulin > 1.2 * (target_basal) || basal_insulin < 0.8 * (target_basal)) {
+  if ( basal_insulin > 50 * (target_basal) || basal_insulin < 1 * (target_basal)) {
 
     var retVal = confirm("Are you sure that's correct? That number is outside the standard range for Basal Insulin.");
     if( retVal === true ){
